92e19d6fbb06b76b1ee16b320cb46d9c861b5746
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2020-07-26 01:54:48 -0400

    Cards styled and structured
    The todays report cards are as in title.
    
    Not hooked up yet to the back end, will structure the rest of the site before doing the back

--- src/Components/MiniCard/MiniCard.js
+++ src/Components/MiniCard/MiniCard.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './MiniCard.css';
 
-const MiniCard = () => {
+const MiniCard = (props) => {
     return(
-        <div className={'minicard ' }>
-            <h1></h1>
+        <div className={'minicard' }>
+            <h2>{props.title}</h2>
+            <h1 className={props.id}>{props.count}</h1>
+            <h2>{'Today: ' + props.todays}</h2>
         </div>
     )
 }
--- src/Components/MiniDash/MiniDash.js
+++ src/Components/MiniDash/MiniDash.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import Axios from 'axios';
 
 import './MiniDash.css';
 
@@ -7,16 +7,57 @@ import MiniCard from '../MiniCard/MiniCard.js';
 
 const MiniDash = () => {
     const [card1, setCard1] = useState({
-
+        id: 'c1',
+        title: 'Total Infections',
+        number: '8,512,321',
+        todaysnumber: '-252'
     })
-
-
-
+    const [card2, setCard2] = useState({
+        id:'c2',
+        title: 'Active Infections',
+        number: '8,512,321',
+        todaysnumber: '-252'
+    })
+    const [card3, setCard3] = useState({
+        id: 'c3',
+        title: 'Recovered',
+        number: '8,512,321',
+        todaysnumber: '-252'
+    })
+    const [card4, setCard4] = useState({
+        id: 'c4',
+        title: 'Deaths',
+        number: '8,512,321',
+        todaysnumber: '-252'
+    })
 
     // Return
     return(
         <div className="minidash">
-            <MiniCard />
+            <MiniCard 
+                title={card1.title}
+                count={card1.number}
+                todays={card1.todaysnumber}
+                id={card1.id}
+            />
+            <MiniCard 
+                title={card2.title}
+                count={card2.number}
+                todays={card2.todaysnumber}
+                id={card2.id}
+            />
+            <MiniCard 
+                title={card3.title}
+                count={card3.number}
+                todays={card3.todaysnumber}
+                id={card3.id}
+            />
+            <MiniCard 
+                title={card4.title}
+                count={card4.number}
+                todays={card4.todaysnumber}
+                id={card4.id}
+            />
         </div>
     )
 }

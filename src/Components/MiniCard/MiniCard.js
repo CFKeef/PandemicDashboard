e133c3ed3a3c00b831e54a53a0827d4469897e2c
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2020-07-29 17:29:57 -0400

    Fixed the report cards
    Fixed the padding issue on the cards

--- src/Components/MiniCard/MiniCard.js
+++ src/Components/MiniCard/MiniCard.js
@@ -5,9 +5,11 @@ import './MiniCard.css';
 const MiniCard = (props) => {
     return(
         <div className={'minicard' }>
-            <h2>{props.title}</h2>
-            <h1 className={props.id}>{props.count}</h1>
-            <h2>{'Today: ' + props.todays}</h2>
+            <div className='textcontainer'>
+                <h2>{props.title}</h2>
+                <h1 className={props.id}>{props.count}</h1>
+                <h2>{'Today: ' + props.todays}</h2>
+            </div>
         </div>
     )
 }

1056782a042d98705a3331e69a3c8f4971879b46
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2020-08-01 16:08:42 -0400

    Graphs back boys!
    Got recharts up and going. Original issue was mapping the data correctly but i solved that for victory so it was a quick set up. Just need to style it now

--- src/Components/Graph/Graph.js
+++ src/Components/Graph/Graph.js
@@ -1,21 +1,33 @@
 import React from 'react';
-
+import * as d3t from 'd3-time-format';
+import { LineChart, Line, CartesianGrid, YAxis, Tooltip, XAxis } from 'recharts';
 
 import './Graph.css';
 
 const Graph = (props) => {
     const processData = () => {
-        return props.graphData.map(element => ({
-            x: element.x,
-            y: element.y
-        }))
+        let timeForm = d3t.timeFormat("%m/%d");
+        let data = props.graphData.map(element => ({
+            dates: timeForm(new Date(element.x)),
+            cases: element.y
+        }));
+
+        return data;
     }
+
+    let data = processData();
+
     return (
         <div className='graph'>
             <p>{props.title}</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            
-        
+            <LineChart width={586} height={480} data={data} style={{ "width": "100%", "margin": "0 auto" }}>
+                <Line type="montone" dataKey="cases" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
         </div>
     )
 }

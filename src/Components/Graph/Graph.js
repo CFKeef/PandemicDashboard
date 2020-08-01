9d48caa0e13ddef489512b3fc324783f1e0d37c5
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2020-08-01 14:55:19 -0400

    Removed Graph
    Going to use a different solution instead.
    React-vis docs blow

--- src/Components/Graph/Graph.js
+++ src/Components/Graph/Graph.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import {XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis} from 'react-vis';
- 
+import React from 'react';
+
+
 import './Graph.css';
 
 const Graph = (props) => {
@@ -15,23 +15,7 @@ const Graph = (props) => {
             <p>{props.title}</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             
-             <XYPlot 
-                height={400} 
-                width={400} 
-                xType='ordinal' 
-                data={props.graphData}
-            >
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                
-                <XAxis 
-                    title='Dates'
-                />
-                <YAxis 
-                    title='Cases'
-                />
-                <LineSeries data={processData()} /> 
-            </XYPlot>  
+        
         </div>
     )
 }
